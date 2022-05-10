ca07444c1d0cdc4be45533d9d7f7150ec90133e1
Ayushi <ayushi@tradebuilderinc.com>
2022-05-10 19:07:33 +0530

    resolve conflit

--- src/app/pages/patient/patient-routing.module.ts
+++ src/app/pages/patient/patient-routing.module.ts
@@ -22,25 +22,11 @@ const routes: Routes = [
       {
         path: 'provider',
         loadChildren: () =>
-          import('@pages/patient/provider/provider.module').then(
-            (m) => m.ProviderModule
-          ),
-      },
-      {
-        path: 'provider/add',
-        loadChildren: () =>
           import(
-            '@pages/patient/provider/add-provider/add-provider.module').then(
-              (m) => m.AddProviderModule
+            '@pages/patient/provider/provider.module').then(
+              (m) => m.ProviderModule
             ),
       },
-			{
-				path: 'provider/edit/:id',
-				loadChildren: () =>
-					import(
-						'@pages/patient/provider/edit-provider/edit-provider.module'
-					).then((m) => m.EditProviderModule),
-			},
       {
         path: 'referer',
         loadChildren: () =>
@@ -50,65 +36,11 @@ const routes: Routes = [
             ),
       },
       {
-        path: 'referer/add',
-        loadChildren: () =>
-          import(
-            '@pages/patient/referer/add-referer/add-referer.module').then(
-              (m) => m.AddRefererModule
-            ),
-      },
-			{
-				path: 'referer/edit/:id',
-				loadChildren: () =>
-					import(
-						'@pages/patient/referer/edit-referer/edit-referer.module'
-					).then((m) => m.EditRefererModule),
-			},
-      {
-        path: 'family_members',
-        loadChildren: () =>
-          import(
-            '@pages/patient/family-member/family-member.module').then(
-              (m) => m.FamilyMemberModule
-            ),
-      },
-      {
-        path: 'family_members/add',
-        loadChildren: () =>
-          import(
-            '@pages/patient/family-member/add-family-member/add-family-member.module').then(
-              (m) => m.AddFamilyMemberModule
-            ),
-      },
-			{
-				path: 'family_members/edit/:id',
-				loadChildren: () =>
-					import(
-						'@pages/patient/family-member/edit-family-member/edit-family-member.module'
-					).then((m) => m.EditFamilyMemberModule),
-			},
-      {
         path: 'legal-guardian',
         loadChildren: () =>
           import(
             '@pages/patient/legal-guardian/legal-guardian.module').then(
               (m) => m.LegalGuardianModule
-            ),
-      },
-      {
-        path: 'legal-guardian/add',
-        loadChildren: () =>
-          import(
-            '@pages/patient/legal-guardian/add-legal-guardian/add-legal-guardian.module').then(
-              (m) => m.AddLegalGuardianModule
-            ),
-      },
-      {
-        path: 'legal-guardian/edit/:id',
-        loadChildren: () =>
-          import(
-            '@pages/patient/legal-guardian/edit-legal-guardian/edit-legal-guardian.module').then(
-              (m) => m.EditLegalGuardianModule
             ),
       },
       {
@@ -120,22 +52,6 @@ const routes: Routes = [
             ),
       },
       {
-        path: 'payment-party/add',
-        loadChildren: () =>
-          import(
-            '@pages/patient/payment-party/add-payment-party/add-payment-party.module').then(
-              (m) => m.AddPaymentPartyModule
-            ),
-      },
-      {
-        path: 'payment-party/edit/:id',
-        loadChildren: () =>
-          import(
-            '@pages/patient/payment-party/edit-payment-party/edit-payment-party.module').then(
-              (m) => m.EditPaymentPartyModule
-            ),
-      },
-      {
         path: 'insurance-subscriber',
         loadChildren: () =>
           import(
@@ -144,20 +60,13 @@ const routes: Routes = [
             ),
       },
       {
-        path: 'insurance-subscriber/add',
+        path: 'insurance',
         loadChildren: () =>
           import(
-            '@pages/patient/insurance-subscriber/add-insurance-subscriber/add-insurance-subscriber.module').then(
-              (m) => m.AddInsuranceSubscriberModule
+            '@pages/patient/insurance/insurance.module').then(
+              (m) => m.InsuranceModule
             ),
       },
-			{
-				path: 'insurance-subscriber/edit/:id',
-				loadChildren: () =>
-					import(
-						'@pages/patient/insurance-subscriber/edit-insurance-subscriber/edit-insurance-subscriber.module'
-					).then((m) => m.EditInsuranceSubscriberModule),
-			},
     ],
   },
 ];
