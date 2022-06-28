0da2653dc2c04ccf47bf0d9522ea54e3a397edf3
gurpinder <gurpinder@tradebuilderinc.com>
2022-06-28 15:58:58 +0530

    sign out update

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -4,24 +4,35 @@ import { CONFIG } from '@config/index';
 import { CookieService } from 'ngx-cookie-service';
 
 @Injectable({
-  providedIn: 'root',
+	providedIn: 'root'
 })
 export class AuthService {
-  constructor(private cookieService: CookieService, private http: HttpClient) {}
-  isLoggedIn() {
-    const session = this.cookieService.get('isLoggedIn');
-    if (session) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-  logout() {
-    return this.http.post(
-      `${CONFIG.backend.host}/auth/api/v1/auth/usr/signout`,
-      {
-        signedOutFromAllDevices: true,
-      }
-    );
-  }
+	constructor(
+		private cookieService: CookieService,
+		private http: HttpClient
+	) {}
+	isLoggedIn() {
+		const session = this.cookieService.get('isLoggedIn');
+		if (session) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	getOrgId() {
+		return this.cookieService.get('orgId');
+	}
+	logout() {
+		return this.http.post(
+			`${CONFIG.backend.host}/auth/signout`,
+			{
+				signOutFromAllDevices: true
+			},
+			{
+				headers: {
+					'X-ORG-ID': this.getOrgId()
+				}
+			}
+		);
+	}
 }
