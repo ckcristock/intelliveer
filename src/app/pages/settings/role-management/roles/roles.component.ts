b4c45042a897a3dce992b00f55734641e3a8e7bf
Ayushi <ayushi@tradebuilderinc.com>
2022-07-04 10:20:25 +0530

    manage role

--- src/app/pages/settings/role-management/roles/roles.component.ts
+++ src/app/pages/settings/role-management/roles/roles.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { RolesUsersService } from "@services/roles-users/roles-users.service";
+import { NavigationExtras, Router } from '@angular/router';
+import { AlertService } from '@services/alert/alert.service';
 import { GlobalRoutesService } from "@services/global-routes/global-routes.service";
+import { RoleService } from '@services/role/role.service';
 
 @Component({
   selector: 'app-roles',
@@ -10,32 +11,52 @@ import { GlobalRoutesService } from "@services/global-routes/global-routes.servi
 })
 export class RolesComponent implements OnInit {
 
-  roles: any [] = [];
+  roleList: any[] = [];
 
   constructor(private router: Router,
-    private rolesUserServ: RolesUsersService,
+    private roleService: RoleService,
+    private alertService: AlertService,
     private globalRoutes: GlobalRoutesService) { }
 
   ngOnInit(): void {
-    this.rolesUserServ.getRoles().subscribe(
-      (resp: any) => {
-        this.roles = resp;
-      }
-    );
+    this.getRoleList();
+  }
+
+  getRoleList()
+  {
+    this.roleService.getRoleList().subscribe((list: any) =>
+    {
+      console.log(list)
+      this.roleList = list;
+    })
   }
 
   addRole() {
-    //this.router.navigate(['/dashboard/settings/role-management/manage-role/add']);
-    this.router.navigate([this.globalRoutes.getSettingsRoleManageRoutes()[1].child[0].url]);
+    this.router.navigate(['/dashboard/settings/role-management/manage-role/add']);
+    // this.router.navigate([this.globalRoutes.getSettingsRoleManageRoutes()[1].child[0].url]);
   }
 
-  gotoEdit() {
-    //this.router.navigate(['/dashboard/settings/role-management/manage-role/edit']);
-    this.router.navigate([this.globalRoutes.getSettingsRoleManageRoutes()[1].child[1].url]);
+  gotoEdit(id: any) {
+    this.router.navigate(['/dashboard/settings/role-management/manage-role/edit/' + id]);
+    // this.router.navigate([this.globalRoutes.getSettingsRoleManageRoutes()[1].child[1].url + "/" + id]);
   }
 
-  deleteRole(id:number){
-    this.rolesUserServ.deleteRole(id);
+  deleteRole(roleId: string) 
+  {
+    this.alertService.conformAlert('Are you sure?', 'You want to delete a role')
+      .then((result: any) => {
+        if (result.value) {
+          this.roleService.deleteRole(roleId).subscribe((data: any) => {
+            this.alertService.success(
+              'Success',
+              'Role has been delete successfully'
+            );
+            this.getRoleList();
+          }, error => {
+            console.log(error)
+          });
+        }
+      });
   }
 
 }
