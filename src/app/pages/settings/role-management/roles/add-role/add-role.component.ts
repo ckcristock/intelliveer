a3c0cd7cf9c7bf516b79cf3cfeb3e4781df31918
Ayushi <ayushi@tradebuilderinc.com>
2022-07-14 20:16:57 +0530

    manage role

--- src/app/pages/settings/role-management/roles/add-role/add-role.component.ts
+++ src/app/pages/settings/role-management/roles/add-role/add-role.component.ts
@@ -61,7 +61,7 @@ export class AddRoleComponent implements OnInit {
 		// 	: (this.displayCreateRoleYesNoOption = false);
 		this.createRoleTemplete = 'yes';
 		this.initForm(this.formData);
-		this.getRoleList();
+		// this.getRoleList();
 		this.getLegelEntityList();
 		this.getLocationList();
 		this.getPracticeList();
@@ -71,6 +71,7 @@ export class AddRoleComponent implements OnInit {
 				if(list.length)
 				{
 					this.bgName = list[0]._id;
+					console.log(list);
 				}
 			});
 
@@ -371,6 +372,24 @@ export class AddRoleComponent implements OnInit {
 		);
 	}
 
+	getRoleListSpecific()
+	{
+		this.roleService.getSpecificRoleTemplateList(this.orgId).subscribe((specificList: any) =>
+			{
+				this.roleService.getPublicRoleTemplateList().subscribe((list: any) =>
+					{
+						this.roleTemplateList =  specificList.concat(list);
+					}, error =>
+					{
+						console.log(error)
+					})
+			}, error =>
+			{
+				console.log(error)
+			})
+			
+	}
+
 	/** Get Selected Org Id */
 	getSelectedBusinessGroupId(){
 		this.businessGroupDropdownService.businessGroup().subscribe((res) => {
@@ -383,15 +402,18 @@ export class AddRoleComponent implements OnInit {
 					this.addRoleTitle = "Create Role from Role Template";
 					this.roleTemplatePlaceholder = "Select role template";
 					this.displayCreateRoleYesNoOption = false;
+					this.getRoleListSpecific();
 				}else{
 					this.addRoleTitle = "Create Role";
 					this.roleTemplatePlaceholder = "Role template name";
 					this.displayCreateRoleYesNoOption = true;
+					this.getRoleList();
 				}
 			}else{
 				this.addRoleTitle = "Create Role from Role Template";
 				this.roleTemplatePlaceholder = "Select role template";
 				this.displayCreateRoleYesNoOption = false;
+				this.getRoleListSpecific();
 			}
 		  });
 	}
