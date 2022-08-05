47cb642af4e1ddb421e86b45aee512561726ad00
Ayushi <ayushi@tradebuilderinc.com>
2022-08-05 16:35:31 +0530

    payment option

--- src/app/pages/patient/consultation/payment-options/payment-options.component.ts
+++ src/app/pages/patient/consultation/payment-options/payment-options.component.ts
@@ -1,98 +1,109 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
-  selector: 'app-payment-options',
-  templateUrl: './payment-options.component.html',
-  styleUrls: ['./payment-options.component.scss']
+	selector: 'app-payment-options',
+	templateUrl: './payment-options.component.html',
+	styleUrls: ['./payment-options.component.scss']
 })
 export class PaymentOptionsComponent implements OnInit {
+	prerequisitesArray: any[] = ['Cavity Clearance', 'CT Scan'];
+	treatmentStepArray: any[] = [
+		'Tooth #3: Maxillary right lateral in Oral Surgeon on bonding',
+		'Restoations #7, #10 by Dentist after Braces removal'
+	];
 
+	txFees: any[] = [
+		{
+			subtitle: 'Final cost to do treatment',
+			content: '$2,567.50'
+		},
+		{
+			subtitle: 'Estimated Insurance Coverage',
+			content: '$2,500.00'
+		},
+		{
+			subtitle: "Patient's Responsibility",
+			content: '$2,567.50'
+		}
+	];
 
-  numberofColums: any [] = [
-    {index: 1},
-    {index: 2}
-  ];
-  txOptions: any[] = [
-    {
-      subtitle: 'Timing',
-      content: 'Observation: 3 month'
-    },
-    {
-      subtitle: 'Treament Phase',
-      content: 'Comprehenssive'
-    },
-    {
-      subtitle: 'Treament Type',
-      content: 'Braces'
-    },
-    {
-      subtitle: 'Treament Time',
-      content: '30-36 Months'
-    },
-    {
-      subtitle: 'Prerequisites',
-      content: 'Cavity Clearance'
-    },
-    {
-      subtitle: 'Extractions',
-      content: 'Tooth #3: Maxillary right lateral incisor'
-    },
-  ];
-  
-  txFees: any[] = [
-    {
-      subtitle: 'Final cost to do treatment',
-      content: '$2,567.50'
-    },
-    {
-      subtitle: 'Estimated Insurance Coverage',
-      content: '$2,500.00'
-    },
-    {
-      subtitle: "Patient's Responsibility",
-      content: '$2,567.50'
-    },
-  ];
+	minDownPayms: any[] = [
+		{
+			subtitle: '',
+			content: '',
+			type: 'dropdown'
+		},
+		{
+			subtitle: 'Min. Down Payment',
+			content: '$500',
+			type: ''
+		},
+		{
+			subtitle: 'Max. Number of Installments',
+			content: '22',
+			type: ''
+		},
+		{
+			subtitle: 'Monthly Installments',
+			content: '$250',
+			type: ''
+		}
+	];
+	showBestOptionCss: boolean = false;
+	paymentPlanType: any[] = [
+		'Payment in full',
+		'Inoffice payment plan - no interest',
+		'Inoffice financing - with interest',
+		'3rd party financing'
+	];
+	compareAllOptionArray: any[] = [
+		{
+			count: 0,
+			locked: true
+		}
+	];
+  splitPayment: boolean = false;
 
-  minDownPayms: any[] = [
-    {
-      subtitle: '',
-      content: '',
-      type: "dropdown"
-    },
-    {
-      subtitle: 'Min. Down Payment',
-      content: '$500',
-      type: ""
-    },
-    {
-      subtitle: "Max. Number of Installments",
-      content: '22',
-      type: ""
-    },
-    {
-      subtitle: "Monthly Installments",
-      content: '$250',
-      type: ""
-    },
-  ];
+	constructor(private modalService: NgbModal) {}
 
-  constructor() { }
+	ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
+	save(data: any) {}
+	cancel() {}
 
-  save(data: any) {
-	}
-	cancel() {
+	checkOdd(i: number) {
+		if (i % 2 == 0) {
+			return false;
+		} else {
+			return true;
+		}
 	}
 
-  checkOdd(i: number){
-    if (i % 2 == 0) {
-      return false;
-    } else {
-      return true;
-    }
+	countTreatment(Obj: any) {
+		if (isNaN(Obj.count)) {
+			Obj.count = 1;
+		} else {
+			Obj.count = Obj.count + 1;
+			if (Obj.count == 3) {
+				this.showBestOptionCss = true;
+			} else {
+				this.showBestOptionCss = false;
+			}
+		}
+	}
+
+  addMoreOption()
+  {
+    this.compareAllOptionArray.push(
+      {
+        count: 0,
+        locked: false
+      }
+    );
   }
 
+	openModel(content: any) {
+		this.modalService.open(content, { centered: true });
+	}
 }
