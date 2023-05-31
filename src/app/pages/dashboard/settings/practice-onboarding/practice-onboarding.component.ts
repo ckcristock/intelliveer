49987e747bd3d884bfd2d609e7cafeb60e2fff17
Cristian <ckcristock@gmail.com>
2023-05-31 04:07:18 -0500

    22

---
+++ src/app/pages/dashboard/settings/practice-onboarding/practice-onboarding.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-practice-onboarding',
+  templateUrl: './practice-onboarding.component.html',
+  styleUrls: ['./practice-onboarding.component.scss']
+})
+export class PracticeOnboardingComponent {
+
+}
--- src/app/pages/dashboard/settings/settings.module.ts
+++ src/app/pages/dashboard/settings/settings.module.ts
@@ -3,11 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { SettingsRoutingModule } from './settings-routing.module';
 import { SettingsMenuComponent } from './settings-menu/settings-menu.component';
+import { OnboardingComponent } from './onboarding/onboarding.component';
+import { RoleManagementComponent } from './role-management/role-management.component';
+import { UserManagementComponent } from './user-management/user-management.component';
+import { PracticeOnboardingComponent } from './practice-onboarding/practice-onboarding.component';
 
 
 @NgModule({
   declarations: [
-    SettingsMenuComponent
+    SettingsMenuComponent,
+    OnboardingComponent,
+    RoleManagementComponent,
+    UserManagementComponent,
+    PracticeOnboardingComponent
   ],
   imports: [
     CommonModule,
