dd06cc2f89f4672797352ac702d3349e510dacd2
Cristian <ckcristock@gmail.com>
2023-06-05 02:59:18 -0500

    36

--- src/app/modules/settings/settings-table/settings-table.component.ts
+++ src/app/modules/settings/settings-table/settings-table.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-settings-table',
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./settings-table.component.scss']
 })
 export class SettingsTableComponent {
+
+  @Input() moduleData: any = undefined;
+
 
 }
