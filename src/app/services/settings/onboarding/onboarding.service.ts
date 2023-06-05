47fcc7eb6bfcda057cb44f3ccacbda259bbe79f6
Cristian <ckcristock@gmail.com>
2023-06-05 02:03:00 -0500

    33

--- src/app/services/settings/onboarding/onboarding.service.ts
+++ src/app/services/settings/onboarding/onboarding.service.ts
@@ -5,63 +5,39 @@ import { Injectable } from '@angular/core';
 })
 export class OnboardingService {
 
-  private leftMenu: string[] = ["Business Group",
-    "Legal Entity", "Location", "Practice"];
-  private goBack: any = "Settings";
-  private rightHeaders: string[] = ["ID",
-    "Name", "Contact Person", "Phone no.", "Created on"];
-  private tableContent: any[] = [
-    {
-      id: "BG1", name: "Business Group 1", contactPerson: "John Doe", phone: "123-456-7890",
-      createdOn: "22 Jan 2021"
-    },
-    {
-      id: "BG1", name: "Business Group 1", contactPerson: "John Doe", phone: "123-456-7890",
-      createdOn: "22 Jan 2021"
-    },
-    {
-      id: "BG2", name: "Business Group 2", contactPerson: "John Doe", phone: "123-456-7890",
-      createdOn: "22 Jan 2021"
-    },
-    {
-      id: "BG3", name: "Business Group 3", contactPerson: "John Doe", phone: "123-456-7890",
-      createdOn: "22 Jan 2021"
-    },
-    {
-      id: "BG4", name: "Business Group 4", contactPerson: "John Doe", phone: "123-456-7890",
-      createdOn: "22 Jan 2021"
-    }
-  ];
-  private onboardingData: any[] = [];
+  private onboardingData: any = {
+    leftMenu: ["Business Group",
+      "Legal Entity", "Location", "Practice"],
+    goBack: "Settings",
+    rightHeaders: ["ID",
+      "Name", "Contact Person", "Phone no.", "Created on"],
+    tableContent: [
+      {
+        id: "BG1", name: "Business Group 1", contactPerson: "John Doe", phone: "123-456-7890",
+        createdOn: "22 Jan 2021"
+      },
+      {
+        id: "BG1", name: "Business Group 1", contactPerson: "John Doe", phone: "123-456-7890",
+        createdOn: "22 Jan 2021"
+      },
+      {
+        id: "BG2", name: "Business Group 2", contactPerson: "John Doe", phone: "123-456-7890",
+        createdOn: "22 Jan 2021"
+      },
+      {
+        id: "BG3", name: "Business Group 3", contactPerson: "John Doe", phone: "123-456-7890",
+        createdOn: "22 Jan 2021"
+      },
+      {
+        id: "BG4", name: "Business Group 4", contactPerson: "John Doe", phone: "123-456-7890",
+        createdOn: "22 Jan 2021"
+      }
+    ]
+  };
 
   constructor() { }
 
   getOnboardingData() {
-    this.leftMenu.forEach((elemento) => {
-      this.onboardingData.push({ leftMenu: elemento });
-    });
-    this.onboardingData.push({ goBack: this.goBack });
-    this.rightHeaders.forEach((elemento) => {
-      this.onboardingData.push({ rightHeaders: elemento });
-    });
-    this.tableContent.forEach((elemento) => {
-      this.onboardingData.push({ tableContent: elemento });
-    });
-
     return this.onboardingData;
-
-  }
-
-  getLeftMenu() {
-    return this.leftMenu;
-  }
-  getGoBack() {
-    return this.goBack;
-  }
-  getRightHeaders() {
-    return this.rightHeaders;
-  }
-  getTableContent() {
-    return this.tableContent;
   }
 }
