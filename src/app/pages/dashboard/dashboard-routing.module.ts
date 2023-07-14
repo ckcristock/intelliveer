996053b12e29f46cef084e9ed9273e5e49c5807b
Cristian <ckcristock@gmail.com>
2023-07-14 10:08:11 -0500

    1 - settings working

--- src/app/pages/dashboard/dashboard-routing.module.ts
+++ src/app/pages/dashboard/dashboard-routing.module.ts
@@ -8,7 +8,39 @@ const routes: Routes = [
 		path: '',
 		component: DashboardComponent,
 		children: [
-
+			{
+				path: 'settings',
+				loadChildren: () =>
+					import('@pages/settings/settings.module').then(
+						(m) => m.SettingsModule
+					)
+			},
+			{
+				path: 'home',
+				loadChildren: () =>
+					import('@pages/home/home.module').then((m) => m.HomeModule)
+			},
+			{
+				path: 'patient',
+				loadChildren: () =>
+					import('@pages/patient/patient.module').then(
+						(m) => m.PatientModule
+					)
+			},
+			{
+				path: 'header',
+				loadChildren: () =>
+					import('@pages/header/header.module').then(
+						(m) => m.HeaderModule
+					),
+			},
+			{
+				path: 'practice-tool',
+				loadChildren: () =>
+					import('@pages/practice-tool/practice-tool.module').then(
+						(m) => m.PracticeToolModule
+					),
+			},
 		]
 	}
 ];
@@ -17,4 +49,4 @@ const routes: Routes = [
 	imports: [RouterModule.forChild(routes)],
 	exports: [RouterModule]
 })
-export class DashboardRoutingModule { }
+export class DashboardRoutingModule {}
--- src/app/pages/settings/settings-routing.module.ts
+++ src/app/pages/settings/settings-routing.module.ts
@@ -7,7 +7,7 @@ const routes: Routes = [
   {
     path: '',
     component: SettingsComponent,
-    canActivate: [RoleGuardGuard],
+    // canActivate: [RoleGuardGuard],
     data:{
       isEnabled: true, 
       value: "",
